bcf8db884b5a50ca9ab6bb869f282818ce6591e5
Arifuzzaman Tusar <arifuzzamantusar50@gmail.com>
2022-02-28 20:38:42 +0600

    random user table

--- random-user/app.js
+++ random-user/app.js
@@ -1,9 +1,25 @@
-const loadApidata = () =>{
-    fetch('https://api.kanye.rest/')
-    .then(Response => Response.json())
-    .then(data => showQuote(data));
-}
+const loadApidata = () => {
+  fetch("https://randomuser.me/api/?results=50")
+    .then((Response) => Response.json())
+    .then((data) => showApiData(data));
+};
 
-const showQuote = quote =>{
-    document.getElementById('quotes').innerText= quote.quote;
-}
+const showApiData = (allusers) => {
+  const contentBox = document.getElementById("content");
+  users = allusers.results;
+
+  users.forEach((user) => {
+    const template = `
+            <td><img src="${user.picture.large}" alt="" srcset=""></td>
+            <td>${user.name.title}</td>
+            <td>${user.name.first+" "+user.name.last}</td>
+            <td>${user.email}</td>
+            <td>${user.location.country}</td>
+          
+        `;
+    const contentBox = document.getElementById("content");
+    const div = document.createElement("tr");
+    div.innerHTML=template;
+    contentBox.appendChild(div);
+  });
+};
